32aa288b6d0f55496f9eb66622f1c92eb3287c52
sangnh99 <sangnh99@gmail.com>
2022-01-20 18:56:01 +0800

    update 2

--- src/main/java/com/example/demodatn/service/TransactionServiceImpl.java
+++ src/main/java/com/example/demodatn/service/TransactionServiceImpl.java
@@ -8,7 +8,7 @@ import com.example.demodatn.repository.FoodRepository;
 import com.example.demodatn.repository.TransactionItemRepository;
 import com.example.demodatn.repository.TransactionRepository;
 import com.example.demodatn.util.StringUtils;
-import org.checkerframework.checker.units.qual.A;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
